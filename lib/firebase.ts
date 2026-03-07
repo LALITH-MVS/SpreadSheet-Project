94b3943de564c939bb2d924d65265c0488f26242
Vibha <LALITH-MVS>
2026-03-07 19:14:12 +0530

    firebase setup completed

---
+++ lib/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAPt0o-qHLF7kdPUdj4OFtmeXGiIaFrsfY",
+  authDomain: "spreadsheet-b106c.firebaseapp.com",
+  projectId: "spreadsheet-b106c",
+  storageBucket: "spreadsheet-b106c.firebasestorage.app",
+  messagingSenderId: "808122160190",
+  appId: "1:808122160190:web:64e8d5050dd108d6a66d6f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
